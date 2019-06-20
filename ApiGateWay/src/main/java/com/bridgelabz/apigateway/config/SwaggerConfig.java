b57194ed75ba8900d3362809724c5f0299c69a8e
tasifmd <tasifmd96@gmail.com>
2019-06-20 19:02:59 +0530

    microservices done

--- ApiGateWay/src/main/java/com/bridgelabz/apigateway/config/SwaggerConfig.java
+++ ApiGateWay/src/main/java/com/bridgelabz/apigateway/config/SwaggerConfig.java
@@ -12,10 +12,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 	@Bean
-    public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.bridgelabz.apigateway"))
-                .build();
+	public Docket apiDocket() {
+	    return new Docket(DocumentationType.SWAGGER_2)
+	            .select()
+	            .apis(RequestHandlerSelectors.basePackage("com.bridgelabz.apigateway"))
+	            .build();
 	}
 }
